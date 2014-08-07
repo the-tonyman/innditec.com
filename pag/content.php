13ab286d44f4f82b8cddfe1a79f30ecb3eec4c14
lorcap88 <lorenzocapuena@innditec.com>
2014-08-07 15:09:16 -0500

    corrigiendo error undefined index para url amigables

--- pag/content.php
+++ pag/content.php
@@ -1,14 +1,19 @@
 <?php
-switch($_GET['pag'])
-{	
-	case "servicios": include ('pag/servicios.php'); break;	
-	case "que-dicen-nuestros-clientes": include ('pag/testimonios.php'); break;
-	case "contactanos": include ('pag/contacto.php'); break;
-	case "conocenos": include ('pag/conocenos.php'); break;
-	case "portafolio": include ('pag/portafolio.php'); break;
-	case "capacitaciones": include ('pag/capacitacion.php'); break;	
-	case "soporte-tecnico": include ('pag/soporte.php'); break;	
-	case "desarrollo-web": include ('pag/desarrollo.php'); break;
-	default : include ('pag/portada.php');
-}
+
+    $pag = isset($_GET['pag']) ? $_GET['pag'] : NULL;
+
+//    switch ($_GET['pag'])
+    switch ($pag)
+    {	
+            case "servicios": include ('pag/servicios.php'); break;	
+            case "que-dicen-nuestros-clientes": include ('pag/testimonios.php'); break;
+            case "contactanos": include ('pag/contacto.php'); break;
+            case "conocenos": include ('pag/conocenos.php'); break;
+            case "portafolio": include ('pag/portafolio.php'); break;
+            case "capacitaciones": include ('pag/capacitacion.php'); break;	
+            case "soporte-tecnico": include ('pag/soporte.php'); break;	
+            case "desarrollo-web": include ('pag/desarrollo.php'); break;
+            default : include ('pag/portada.php');
+    }
+
 ?>
