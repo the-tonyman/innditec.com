48f429e02c37cd295fd64d5cc51f2d0cb33ea31f
the-tonyman <anthonyramirez@innditec.com>
2014-08-14 18:18:18 -0500

    cambio prueba

--- Admin/lib/conexion.php
+++ Admin/lib/conexion.php
@@ -5,9 +5,9 @@
         public static function Conexion()
         {
             $server = 'localhost';
-            $user = 'root';
-            $password ='';
-            $db = 'innditec_db';
+            $user = 'tonyman';
+            $password ='tonyman';
+            $db = 'db_innditec';
             
             $con = mysql_connect($server,$user,$password)or die("Error de conexion");
             mysql_select_db($db);
